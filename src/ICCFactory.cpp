ac1e976b8948515bf7832a6b4140cfd8550d9046
Omkar Athavale <oa1234@live.com>
2021-12-12 09:19:34 +0000

    excitability gradient

--- src/ICCFactory.cpp
+++ src/ICCFactory.cpp
@@ -4,13 +4,14 @@ template<unsigned DIM>
 AbstractCardiacCell* ICCFactory<DIM>::CreateCardiacCellForTissueNode(Node<DIM>* pNode)
 {
   unsigned index = pNode->GetIndex();
-  
+  double x = pNode->GetPoint[0]
   if(setICCNode.find(index) != setICCNode.end())
   {
     CellDu2013_neural_sensFromCellML* cell = new CellDu2013_neural_sensFromCellML(this->mpSolver, this->mpZeroStimulus);
     
-    return cell;
+    cell->SetParameter("E_K", -70.0-4.0*x)
 
+    return cell;
   }
 
   return new DummyDerivedCa(this->mpSolver, this->mpZeroStimulus);
